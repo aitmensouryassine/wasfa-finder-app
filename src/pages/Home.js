ea1244332a47c3ca2525da08e6b522b2b493cca7
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2024-03-14 13:58:32 +0000

    Added state to Home component, fetch recipes, and render, + Some changes in style

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,39 @@
-function Home() {
-  return <div>Home</div>;
-}
+import { useEffect, useMemo, useState } from 'react';
+import { mealType as getMealType } from '../utils';
+import Cover from '../components/Cover';
+import Recipes from '../components/Recipes';
+import { getRecipesByMealType } from '../api';
+import '../styles/home.scss';
 
-export default Home;
+export default function Home() {
+  const [mealType, setMealType] = useState(getMealType());
+  const [recipes, setRecipes] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [dependency, setDependency] = useState(0);
+
+  const fetchRecipes = useMemo(
+    () => async () => {
+      try {
+        const data = await getRecipesByMealType();
+        console.log(data.hits);
+        setRecipes(data.hits);
+        setLoading(false);
+      } catch (err) {
+        console.log(err);
+      }
+    },
+    [dependency]
+  );
+
+  useEffect(() => {
+    console.log(mealType);
+    fetchRecipes();
+  }, [fetchRecipes]);
+
+  return (
+    <section className='Home'>
+      <Cover mealType={mealType} />
+      <Recipes recipes={recipes} />
+    </section>
+  );
+}
