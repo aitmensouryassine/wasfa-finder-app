67720a2d13224de3ebe71ded69c9ca7e5502d717
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2024-03-17 14:42:31 +0000

    Added: get recipe by Id, and changed base url

--- src/api/index.js
+++ src/api/index.js
@@ -3,17 +3,18 @@ import { mealType, mergeDuplicateRecipes } from '../utils';
 
 export const api_key = '446aa5a2b7ad7f4594f12336c100a887';
 export const app_id = '3cec3aee';
-export const baseUrl = `https://api.edamam.com/api/recipes/v2?type=public&app_id=${app_id}&app_key=${api_key}`;
+export const baseUrl = `https://api.edamam.com/api/recipes/v2/`;
+export const credentials = `&app_id=${app_id}&app_key=${api_key}`;
 
 export const getRecipesByMealType = async () => {
   try {
-    const { data } = await axios.get(baseUrl + '&mealType=' + mealType());
+    const { data } = await axios.get(`${baseUrl}?type=public${credentials}&mealType=${mealType()}`);
     return {
       recipes: data?.hits || [],
       nextRecipesUrl: data?._links?.next?.href || '',
     };
   } catch (error) {
-    console.log(error);
+    console.error(error);
     return {
       recipes: [],
       nextRecipesUrl: '',
@@ -32,5 +33,15 @@ export const getNextRecipes = async (nextUrl, setRecipes, setNextUrl, setLoading
   } catch (err) {
     setHasMore(false);
     console.error(err);
+  }
+};
+
+export const getRecipeById = async (id) => {
+  try {
+    const response = await axios.get(`${baseUrl}${id}/?type=public${credentials}`);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    return {};
   }
 };
