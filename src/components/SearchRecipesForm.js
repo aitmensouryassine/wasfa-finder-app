47c229098ba3b9667d35341122f136d799b6d5e1
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2024-03-23 15:37:01 +0000

    Redesign: Search Page

---
+++ src/components/SearchRecipesForm.js
@@ -0,0 +1,246 @@
+import { useState } from 'react';
+import RangeSlider from 'react-range-slider-input';
+import 'react-range-slider-input/dist/style.css';
+import '../styles/search-recipes-form.scss';
+
+const dietChoices = [
+  { id: 1, name: 'balanced' },
+  { id: 2, name: 'high-fiber' },
+  { id: 3, name: 'high-protein' },
+  { id: 4, name: 'low-carb' },
+  { id: 5, name: 'low-fat' },
+  { id: 6, name: 'low-sodium' },
+];
+const cuisineTypeChoices = [
+  { id: 1, name: 'American' },
+  { id: 2, name: 'Asian' },
+  { id: 3, name: 'British' },
+  { id: 4, name: 'Caribbean' },
+  { id: 5, name: 'Central Europe' },
+  { id: 6, name: 'Chinese' },
+  { id: 7, name: 'Eastern Europe' },
+  { id: 8, name: 'French' },
+  { id: 9, name: 'Indian' },
+  { id: 10, name: 'Italian' },
+  { id: 11, name: 'Japanese' },
+  { id: 12, name: 'Kosher' },
+  { id: 13, name: 'Mediterranean' },
+  { id: 14, name: 'Mexican' },
+  { id: 15, name: 'Middle Eastern' },
+  { id: 16, name: 'Nordic' },
+  { id: 17, name: 'South American' },
+  { id: 18, name: 'South East Asian' },
+];
+
+const MIN = 1,
+  MAX = 20;
+
+export default function SearchRecipesForm() {
+  const [ingredientsRange, setIngredientsRange] = useState(['', '']);
+  const [minIngredients, setMinIngredients] = useState('');
+  const [maxIngredients, setMaxIngredients] = useState('');
+  const [searchQuery, setSearchQuery] = useState('');
+  const [includeIngredient, setIncludeIngredient] = useState('');
+  const [includeIngredients, setIncludeIngredients] = useState([]);
+  const [excludeIngredient, setExcludeIngredient] = useState('');
+  const [excludeIngredients, setExcludeIngredients] = useState([]);
+  const [cuisineType, setCuisineType] = useState('');
+  const [diet, setDiet] = useState('');
+  const [time, setTime] = useState('');
+
+  const handleRangeChange = (range) => {
+    const [min, max] = range;
+    setMinIngredients(min);
+    setMaxIngredients(max);
+    setIngredientsRange(range);
+  };
+
+  const handleMinIngredient = (evt) => {
+    const value = parseInt(evt.target.value);
+    if (isNaN(value)) {
+      setMinIngredients('');
+    } else if (value >= MIN && value <= 20) {
+      setMinIngredients(value);
+      setIngredientsRange((prevRange) => [value, prevRange[1]]);
+    } else {
+      setMinIngredients(MIN);
+      setIngredientsRange((prevRange) => [MIN, prevRange[1]]);
+    }
+  };
+
+  const handleMaxIngredient = (evt) => {
+    const value = parseInt(evt.target.value);
+    if (isNaN(value)) {
+      setMaxIngredients('');
+    } else if (value >= MIN && value <= 20) {
+      setMaxIngredients(value);
+      setIngredientsRange((prevRange) => [prevRange[0], value]);
+    } else {
+      setMaxIngredients(MAX);
+      setIngredientsRange((prevRange) => [prevRange[0], MAX]);
+    }
+  };
+
+  const addIncludeIngredient = (evt) => {
+    evt.preventDefault();
+    if (includeIngredient && !includeIngredients.includes(includeIngredient)) {
+      setIncludeIngredients((prevState) => [...prevState, includeIngredient]);
+    }
+    setIncludeIngredient('');
+  };
+  const addExcludeIngredient = (evt) => {
+    evt.preventDefault();
+    if (excludeIngredient && !excludeIngredients.includes(excludeIngredient)) {
+      setExcludeIngredients((prevState) => [...prevState, excludeIngredient]);
+    }
+    setExcludeIngredient('');
+  };
+
+  const handleSubmit = () => {
+    console.log('search');
+  };
+
+  return (
+    <div className='SearchRecipesForm'>
+      <h3>Search for a recipe!</h3>
+      <small>You're free to use one or more fields!</small>
+      <div className='form'>
+        <div className='group'>
+          <label htmlFor='search-query'>General search</label>
+          <input name='search-query' type='text' value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} />
+        </div>
+
+        <form onSubmit={addIncludeIngredient} className='include-ingredients-form'>
+          <div className='group'>
+            <label htmlFor='inlude-ingredient'>Include ingredients</label>
+            <div className='inputs'>
+              <input
+                name='inlude-ingredient'
+                type='text'
+                value={includeIngredient}
+                onChange={(e) => setIncludeIngredient(e.target.value)}
+              />
+              <button role='submit'>
+                <i className='bi bi-plus'></i>
+              </button>
+            </div>
+          </div>
+          <div className='included-ingredients'>
+            {includeIngredients.map((ingr, idx) => (
+              <span
+                key={idx}
+                onClick={() =>
+                  setIncludeIngredients((prevState) => prevState.filter((ingredient) => ingredient !== ingr))
+                }
+              >
+                {ingr}
+              </span>
+            ))}
+          </div>
+        </form>
+
+        <form onSubmit={addExcludeIngredient} className='exclude-ingredients-form'>
+          <div className='group'>
+            <label htmlFor='exlude-ingredient'>Exclude ingredients</label>
+            <div className='inputs'>
+              <input
+                name='exlude-ingredient'
+                type='text'
+                value={excludeIngredient}
+                onChange={(e) => setExcludeIngredient(e.target.value)}
+              />
+              <button role='submit'>
+                <i className='bi bi-plus'></i>
+              </button>
+            </div>
+          </div>
+          <div className='excluded-ingredients'>
+            {excludeIngredients.map((ingr, idx) => (
+              <span
+                key={idx}
+                onClick={() =>
+                  setExcludeIngredients((prevState) => prevState.filter((ingredient) => ingredient !== ingr))
+                }
+              >
+                {ingr}
+              </span>
+            ))}
+          </div>
+        </form>
+
+        <div className='group'>
+          <label htmlFor='number-of-ingredients'>Number of ingredients</label>
+          <RangeSlider
+            className='slider'
+            name='number-of-ingredients'
+            min={MIN}
+            max={MAX}
+            step={1}
+            value={ingredientsRange}
+            onInput={handleRangeChange}
+          />
+          <div className='min-max'>
+            <div className='min'>
+              <label htmlFor='min'>Min</label>
+              <input
+                type='number'
+                name='min'
+                value={minIngredients}
+                onChange={handleMinIngredient}
+                min={ingredientsRange[0]}
+                max={ingredientsRange[1]}
+              />
+            </div>
+            <div className='max'>
+              <label htmlFor='max'>Max</label>
+              <input
+                type='number'
+                name='max'
+                value={maxIngredients}
+                onChange={handleMaxIngredient}
+                min={ingredientsRange[0]}
+                max={ingredientsRange[1]}
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className='group'>
+          <label htmlFor='time'>
+            Time <small>(minutes)</small>
+          </label>
+          <input type='number' name='time' value={time} onChange={(evt) => setTime(parseInt(evt.target.value))} />
+        </div>
+
+        <div className='group'>
+          <label htmlFor='cuisine-type'>Cuisine Type</label>
+          <select name='cuisine-type' onChange={(e) => setCuisineType(e.target.value)}>
+            <option></option>
+            {cuisineTypeChoices.map(({ id, name }) => (
+              <option key={id} value={name}>
+                {name}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <div className='group'>
+          <label htmlFor='diet'>Diet</label>
+          <select name='diet' onChange={(e) => setDiet(e.target.value)}>
+            <option></option>
+            {dietChoices.map(({ id, name }) => (
+              <option key={id} value={name}>
+                {name}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className='submit'>
+          <a className='btn' onClick={handleSubmit} href='#search'>
+            Search
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,8 +1,8 @@
-import SearchBar from '../components/SearchBar';
 import { useContext, useState } from 'react';
 import Loading from '../components/Loading';
 import Recipes from '../components/Recipes';
 import RecipeContext from '../context/recipe';
+import SearchRecipesForm from '../components/SearchRecipesForm';
 
 export default function Search() {
   const [term, setTerm] = useState('');
@@ -24,17 +24,21 @@ export default function Search() {
 
   return (
     <section className='Search'>
-      <SearchBar setTerm={setTerm} handleSubmit={handleSubmit} />
-      {loading ? (
-        <Loading />
-      ) : (
-        <Recipes
-          recipes={recipes}
-          setRecipes={setRecipes}
-          nextRecipesUrl={nextRecipesUrl}
-          setNextRecipesUrl={setNextRecipesUrl}
-        />
-      )}
+      <aside>
+        <SearchRecipesForm />
+      </aside>
+      <main id='#search'>
+        {loading ? (
+          <Loading />
+        ) : (
+          <Recipes
+            recipes={recipes}
+            setRecipes={setRecipes}
+            nextRecipesUrl={nextRecipesUrl}
+            setNextRecipesUrl={setNextRecipesUrl}
+          />
+        )}
+      </main>
     </section>
   );
 }
