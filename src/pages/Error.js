d5262c4e567234430e6a9d2706c17a7a901efe16
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2024-03-24 23:40:59 +0000

    Added custom 404 Page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,8 +1,32 @@
+import { useContext } from 'react';
 import { useRouteError } from 'react-router-dom';
+import ThemeContext from '../context/theme';
+import Header from '../components/Header';
+import pizza from '../images/pizza.png';
+import '../styles/error.scss';
 
 function Error() {
+  const { dark } = useContext(ThemeContext);
+
   console.error(useRouteError());
-  return <div>Error Page</div>;
+  return (
+    <div className={`App-${dark ? 'dark' : 'light'}`}>
+      <Header />
+      <section className='Error'>
+        <div className='error-container'>
+          <div className='not-found'>
+            <span>4</span>
+            <span>
+              <img src={pizza} />
+            </span>
+            <span>4</span>
+          </div>
+          <h2>Sorry! The recipe you're looking for isn't available 🥲</h2>
+          <a href='/'>Go To Home Page</a>
+        </div>
+      </section>
+    </div>
+  );
 }
 
 export default Error;
