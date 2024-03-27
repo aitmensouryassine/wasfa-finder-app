7d2f965729f414aa6a1d0c8d87c1b041bea1b4ce
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2024-03-27 23:03:58 +0000

    Added goto the top of page button

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useRef } from 'react';
 import { Outlet } from 'react-router-dom';
 import ThemeContext from './context/theme';
 import Header from './components/Header';
@@ -6,11 +6,34 @@ import './styles/app.scss';
 
 function App() {
   const { dark } = useContext(ThemeContext);
+  const gotoTopBtn = useRef();
+
+  const handleScroll = (evt) => {
+    if (window.scrollY > window.innerHeight * 2) {
+      gotoTopBtn.current.style.display = 'block';
+    } else {
+      gotoTopBtn.current.style.display = 'none';
+    }
+  };
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const gotoTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
 
   return (
     <div className={`App-${dark ? 'dark' : 'light'}`}>
       <Header />
       <Outlet />
+      <div className='goto-top-btn' ref={gotoTopBtn} onClick={gotoTop}>
+        <i className='bi bi-caret-up-square-fill'></i>
+      </div>
     </div>
   );
 }
