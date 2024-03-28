05e8c5b883472bd7792526d1772216a6a34ed5b4
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2024-03-28 15:12:09 +0000

    Added a manifest.json file + Update Footer links to use react-router-dom link + Advanced search button css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,11 @@
+import { Link } from 'react-router-dom';
 import '../styles/footer.scss';
 
 export default function Footer({ device }) {
   return (
     <div className={`Footer ${device}`}>
       <div className='links'>
-        <a href='/terms'>Terms</a> · <a href='/privacy'>Privacy Policy</a>
+        <Link to='/terms'>Terms</Link> · <Link to='/privacy'>Privacy Policy</Link>
       </div>
       <div className='copyright'>WasfaFinder © 2024</div>
     </div>
