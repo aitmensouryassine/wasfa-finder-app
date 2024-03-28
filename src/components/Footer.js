0befbd81dc01b686d469268f0543b0ea3932508e
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2024-03-28 15:38:14 +0000

    Hide Menu when clicking on footer links on mobile

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,17 @@
 import { Link } from 'react-router-dom';
 import '../styles/footer.scss';
 
-export default function Footer({ device }) {
+export default function Footer({ device, handleHide = () => {} }) {
   return (
     <div className={`Footer ${device}`}>
       <div className='links'>
-        <Link to='/terms'>Terms</Link> · <Link to='/privacy'>Privacy Policy</Link>
+        <Link to='/terms' onClick={handleHide}>
+          Terms
+        </Link>{' '}
+        ·{' '}
+        <Link to='/privacy' onClick={handleHide}>
+          Privacy Policy
+        </Link>
       </div>
       <div className='copyright'>WasfaFinder © 2024</div>
     </div>
